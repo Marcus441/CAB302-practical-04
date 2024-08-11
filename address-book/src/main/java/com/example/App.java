149dfc8dba3c8fbeb5d07dcef059a0c4ae0a9c48
Marcus441 <psminchia4153@hotmail.com>
2024-08-11 18:44:50 +1000

    fixed bad import

--- address-book/src/main/java/com/example/App.java
+++ address-book/src/main/java/com/example/App.java
@@ -1,25 +1,26 @@
 package com.example;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class App extends Application {
-  public static final String TITLE = "Address Book";   
-  public static final int WIDTH= 640;
-  public static final int HEIGHT= 360;   
-  
-  @Override
-    public void start(Stage stage) {
-        FXMLLoader fxmlLoader= new FXMLLoader(App.class.getResource("hello-view.fxml"));  
-        Scene scene = new Scene(fxmlLoader, WIDTH, HEIGHT);
-        stage.setScene(scene);
-        stage.setTitle(TITLE); 
-        stage.show();
-    }
+  public static final String TITLE = "Address Book";
+  public static final int WIDTH = 640;
+  public static final int HEIGHT = 360;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+    stage.setTitle(TITLE);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
